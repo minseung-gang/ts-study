8a6738a1981e741dca1968e81e1de202bb9c3ce9
minseung-gang <strause1@nate.com>
2023-05-09 19:17:16 +0900

    Create useCallback login function

--- my-books/src/containers/SigninContainer.tsx
+++ my-books/src/containers/SigninContainer.tsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Signin from "../compoenets/Signin";
-
+import { useDispatch } from "react-redux";
+import { LoginReqType } from "../types";
+import { login as loginSagaStart } from "../redux/modules/auth";
 export default function SigninContainer() {
-  return <Signin />;
+  const dispatch = useDispatch();
+  const login = useCallback(
+    (reqData: LoginReqType) => {
+      dispatch(loginSagaStart(reqData));
+    },
+    [dispatch]
+  );
+  return <Signin login={login} />;
 }
