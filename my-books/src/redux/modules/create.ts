6b3c6f2feb67004d308922d9860c53ec9b47dbb6
minseung-gang <strause1@nate.com>
2023-05-09 19:18:04 +0900

    add comment

--- my-books/src/redux/modules/create.ts
+++ my-books/src/redux/modules/create.ts
@@ -5,6 +5,7 @@ import reducer from "./reducer";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 
+// Redux Saga는 Redux 액션을 리스닝하고, 그에 대응하는 사이드 이펙트를 실행하는 라이브러리.
 const create = () => {
   const sagaMiddleware = createSagaMiddleware();
   const store = createStore(
