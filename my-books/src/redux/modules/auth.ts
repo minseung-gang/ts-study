cb758a1d5ed513d27bdfe7bdb0970b18a7c5da2b
minseung-gang <strause1@nate.com>
2023-05-09 19:17:49 +0900

    Setting up authSaga and loginSaga

--- my-books/src/redux/modules/auth.ts
+++ my-books/src/redux/modules/auth.ts
@@ -1,4 +1,7 @@
-import { createActions, handleActions } from "redux-actions";
+import { createActions, handleActions, Action } from "redux-actions";
+import { takeEvery, put, call } from "redux-saga/effects";
+import { LoginReqType } from "../../types";
+import UserService from "../../services/UserService";
 
 interface AuthState {
   token: string | null;
@@ -21,6 +24,7 @@ export const { pending, success, fail } = createActions(
 );
 
 const reducer = handleActions<AuthState, string>(
+  // handleActions<State, Payload>,
   {
     PENDING: (state) => ({ ...state, loading: true, error: null }),
     SUCCESS: (state, action) => ({
@@ -46,5 +50,28 @@ JavaScript에서 객체 리터럴을 괄호로 감싸주는 경우,
 */
 
 //saga
-export function* authSaga() {}
+export const { login, logout } = createActions("LOGIN", "LOGOUT", { prefix });
+
+function* loginSaga(action: Action<LoginReqType>) {
+  // Action >> Redux 액션 객체
+  try {
+    yield put(pending());
+    const token: string = yield call(UserService.login, action.payload);
+    //localstorage
+    yield put(success(token));
+    //push
+  } catch (error: any) {
+    yield put(fail(new Error(error?.response?.data?.error || "UNKNOWN")));
+  }
+}
+
+function* logoutSaga() {}
+
+export function* authSaga() {
+  yield takeEvery(`${prefix}/LOGIN`, loginSaga); // takeEvery, 액션을 비동기적으로 처리할 때 사용, (액션 타입 문자열 | 액션 타입 배열 , 디스패치될 때 실행할 제너레이터 함수)
+  yield takeEvery(`${prefix}/LOGOUT`, logoutSaga);
+}
+// takeEvery 함수를 사용하면, 비동기적인 작업을 수행하는 액션 처리기 함수들을 작성할 때, 코드의 가독성과 유지보수성을 높일 수 있습니다.
+// 예를 들어, LOGIN 액션이 디스패치될 때마다 loginSaga 함수를 실행하여, 로그인 API를 호출하고, 성공하면 토큰을 저장하고,
+// 실패하면 에러 메시지를 출력하는 등의 작업을 수행할 수 있습니다.
 export default reducer;
